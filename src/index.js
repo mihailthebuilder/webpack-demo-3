022b6728b9a70e7e630dfa1183b32d6b3dc5a083
Mihail Marian <mihailmarian12@gmail.com>
2020-11-09 15:01:24 +0000

    Loading fonts chapter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Logo from './logo.jpeg';
 
 const component = () => {
   const element = document.createElement('div');
@@ -7,7 +8,11 @@ const component = () => {
   //Lodash, currently included via module loaded here
   element.innerHTML = _.join(['Hello','webpack'],' ');
   element.classList.add('hello');
-  
+
+  //add image to existing div
+  const myLogo = new Image();
+  myLogo.src = Logo;
+
   return element;
 }
 
