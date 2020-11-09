a8dc10bc0527fced5d228a1bb1ed37fc5dce6ef3
Mihail Marian <mihailmarian12@gmail.com>
2020-11-09 14:31:31 +0000

    Loading css chapter completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
+import './style.css';
 
 const component = () => {
   const element = document.createElement('div');
 
   //Lodash, currently included via module loaded here
   element.innerHTML = _.join(['Hello','webpack'],' ');
+  element.classList.add('hello');
+  
   return element;
 }
 
