c43a5d6895eb0b94ba3cb2b15c266e9dbdceaf9e
Mihail Marian <mihailmarian12@gmail.com>
2020-11-09 14:14:40 +0000

    finished basic setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const component = () => {
   const element = document.createElement('div');
+
+  //Lodash, currently included via a script inside html file
+  element.innerHTML = _.join(['Hello','webpack'],' ');
+  return element;
 }
+
+document.appendChild(component());
